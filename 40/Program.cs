4c4f42af821c066d1bc329f23cbe2e12bdaba16b
EkaterinaFedotova <katyaF85@yandex.ru>
2023-02-12 10:58:49 +0300

    Задачи 39,40,42,44 семинар

---
+++ 40/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая принимает на вход три числа и проверяет, 
+//может ли существовать треугольник с сторонами такой длины.
+// ❗ **Теорема о неравенстве треугольника:
+// **каждая сторона треугольника меньше суммы двух других сторон.
+
+Console.Clear();
+Console.Write("Введите стороны треугольника через пробел: ");
+
+string[] st = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+int a = int.Parse(st[0]), b = int.Parse(st[1]), c = int.Parse(st[2]);
+
+if ((a + b) > c && (a + c) > b && (b + c) > a)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
---
+++ 44/Program.cs
@@ -0,0 +1,31 @@
+﻿// Не используя рекурсию, выведите первые N чисел Фибоначчи.
+//  Первые два числа Фибоначчи: 0 и 1.
+
+// Если N = 5-> 0 1 1 2 3
+
+// Если N = 3 -> 0 1 1
+
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+// <aside>
+// ❗️ Числа Фибоначчи — числовая ****последовательность,
+//  каждый элемент которой равен сумме двух предыдущих.
+
+
+Console.Clear();
+int number = int.Parse(Console.ReadLine());
+f(number);
+
+void f(int countTests)
+{
+    int fibonacci1 = 0;
+    int fibonacci2 = 1;
+    Console.Write($"f({0}) = {0}; ");
+    for (int i = 1; i < countTests; i++)
+    {
+        Console.Write($"f({i}) = {fibonacci2}; ");
+        int temporary = fibonacci2;
+        fibonacci2 += fibonacci1;
+        fibonacci1 = temporary;
+    }
+}
